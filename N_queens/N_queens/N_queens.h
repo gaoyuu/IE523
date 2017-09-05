5a93b4037b3e910cdb765de896176cc52b590067
gaoyu <gaoyu326@gmail.com>
2017-09-05 11:34:37 -0500

    test
    test

--- N_queens/N_queens/N_queens.h
+++ N_queens/N_queens/N_queens.h
@@ -55,7 +55,7 @@ class Board
     // private member function: initializes the (n x n) chessboard
     void initialize(int n)
     {
-        size = n;
+        //size = n;
         int **chess_board = new int*[n];
         
         for (int i = 0; i < n; i++)
