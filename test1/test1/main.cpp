2b4311485731e094a3481d7b1bff7503648cb48e
gaoyu <gaoyu326@gmail.com>
2017-10-15 14:06:25 -0500

    gege

--- test1/test1/main.cpp
+++ test1/test1/main.cpp
@@ -1,25 +1,5 @@
-//
-//  main.cpp
-//  test1
-//
-//  Created by Yu Gao on 9/7/17.
-//  Copyright © 2017 Yu Gao. All rights reserved.
-//
-
-#include <iostream>
-#include "sudoku_all.h"
-
-using namespace std;
-
-int main (int argc, char * const argv[])
+double get uniform()
 {
-    Sudoku x;
-    x.read_puzzle(argc, argv);
-    cout << "Input file name : " << argv[1] << endl;
-    x.print_puzzle();
-    x.Solve(0,0);
-    //x.alternate_Solve(0, 0);
-    //x.print_puzzle();
-    
-    return 0;
+    return (((double) random())/(pow(2.0, 31.0)-1.0));
 }
+
